6e87fcbe99e78592eb04ea1d28ee5d686901e22e
s-omas <49887549+s-omas@users.noreply.github.com>
2021-06-01 14:13:44 -0600

    fix js

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
             url: "https://raw.githubusercontent.com/s-omas/test-mechanism/master/mechanism_index.json",
             type: 'get',
             success: function(response){
-		sessionStorage.setItem('mechanism_index', response);
-                for (key in response){
+		var m_index = JSON.parse(response)
+		sessionStorage.setItem('mechanism_index', m_index);
+                for (key in m_index){
 			alert(key)
-			var mech_obj = response[key]
+			var mech_obj = m_index[key]
 			var mech_description = mech_obj["description"]
 			$("#m_container").append('<div class="row"><div class="card"><div class="card-body"><h5 class="card-title">' + key + '</h5><p class="card-text">' + mech_description + '</p></div></div></div>');	
 		}
