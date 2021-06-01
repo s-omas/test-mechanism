99a383784064455e755387b7328c91c85c04251d
s-omas <49887549+s-omas@users.noreply.github.com>
2021-06-01 13:29:13 -0600

    pull mech index

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+    $.ajax({
+            url: "https://raw.githubusercontent.com/s-omas/test-mechanism/master/mechanism_index.json",
+            type: 'get',
+            success: function(response){
+                sessionStorage.setItem('mechanism_index', JSON.stringify(response));
+            }
+     });
 });
