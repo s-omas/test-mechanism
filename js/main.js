aab3d0283378cdbfef0b6634989ebb65a14617bc
s-omas <49887549+s-omas@users.noreply.github.com>
2021-06-01 14:03:40 -0600

    js and bootstrap

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@ $(document).ready(function(){
             url: "https://raw.githubusercontent.com/s-omas/test-mechanism/master/mechanism_index.json",
             type: 'get',
             success: function(response){
-                sessionStorage.setItem('mechanism_index', response);
+                for (key in response){
+			sessionStorage.setItem('mechanism_index', response);
+			var mech_obj = response[key]
+			var mech_description = mech_obj["description"]
+			$("#m_container").append('<div class="row"><div class="card"><div class="card-body"><h5 class="card-title">' + key + '</h5><p class="card-text">' + mech_description + '</p></div></div></div>');	
+		}
             }
      });
 });
