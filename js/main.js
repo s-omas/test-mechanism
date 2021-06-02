30d1b22712084e3dd4f5e658e4fec72b24b16c9f
s-omas <49887549+s-omas@users.noreply.github.com>
2021-06-02 09:35:58 -0600

    mechanism fetch

--- js/main.js
+++ js/main.js
@@ -8,21 +8,22 @@ $(document).ready(function(){
                 for (key in m_index){
 			var mech_obj = m_index[key]
 			var mech_description = mech_obj["description"]
-			$("#m_container").append('<div class="row"><div class="card"><div class="card-body"><h5 class="card-title">' + key + '</h5><p class="card-text">' + mech_description + '</p><a id="select_' + key + '" href="#" class="select_mechanism btn btn-primary">Select</a></div></div></div>');	
+			$("#m_container").append('<div class="row"><div class="card"><div class="card-body"><h5 class="card-title">' + key + '</h5><p class="card-text">' + mech_description + '</p><a mechanism="' + key + '" href="#" class="select_mechanism btn btn-primary">Select</a></div></div></div>');	
 		}
             }
      });
      $(".select_mechanism").on('click', function(){
 	     var m_index = sessionStorage.getItem('mechanism_index');
-	     var mechanism_name_dirty = $(this).attr('id')
-	     var mechanism_name = mechanism_name_dirty.split("_")[0]
+	     var mechanism_name = $(this).attr('mechanism')
 	     var m_url = m_index[mechanism_name]["url"]
 	     alert(m_url)
 	     $.ajax({
 	          url: m_url,
                   type: 'get',
                   success: function(response){
-                  }
+                  	var mechanism = JSON.parse(response)
+			sessionStorage.setItem('mechanism', mechanism)
+		  }
      	     });
 
      });	
