95686e466aed679f07ec2ec77965a65fa573679d
s-omas <49887549+s-omas@users.noreply.github.com>
2021-06-02 11:08:28 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 			sessionStorage.setItem('mechanism', response);
 			new_mechanism = JSON.parse(response);
 			for (reaction in new_mechanism["pmc-data"][0]["reactions"]) {
-				$("#mech_box").htnml("")
+				$("#mech_box").html("");
 				$("#mech_box").append('<div class="row"><div class="card"><div class="card-body"><p class="card-text">' + reaction["type"] + '</p></div></div></div>')
 			}
 		}
