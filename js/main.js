c4060382e99ae0bdd9226aad66701d510503d5f2
s-omas <49887549+s-omas@users.noreply.github.com>
2021-06-02 11:04:14 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 			new_mechanism = JSON.parse(response);
 			for (reaction in new_mechanism["pmc-data"][0]["reactions"]) {
 				$("#mech_box").htnml("")
-				$("#mech_box").append('<div class="row"><div class="card"><div class="card-body"><p class="card-text">' + reaction + '</p></div></div></div>')
+				$("#mech_box").append('<div class="row"><div class="card"><div class="card-body"><p class="card-text">' + reaction["type"] + '</p></div></div></div>')
 			}
 		}
 	    });
